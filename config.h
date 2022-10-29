c51e3e3ccfdac10fe07ef5df594ec539a9dde41c
Lew1s777 <lew1s0620@outlook.com>
2022-10-29 15:23:38 +0800

    dmenu replaced with rofi

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 1;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
@@ -18,7 +18,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 //static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
-static const char *fonts[]          = { "Minecraft:size=16" };
+static const char *fonts[]          = { "Minecraft:size=16","Twitter Color Emoji:size=16","Source Code Pro:size=16"};
 static const char dmenufont[]       = "Minecraft:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 //	{ "Tile",      tile },    /* first entry is default */
 //	{ "><>",      NULL },    /* no layout function means floating behavior */
 //	{ "[M]",      monocle },
-	{ "][",      tile },    /* first entry is default */
+	{ "[]",      tile },    /* first entry is default */
 	{ "><",      NULL },    /* no layout function means floating behavior */
 	{ "[]",      monocle },
 };
@@ -86,10 +86,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0";                                          /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "st", NULL };
+//static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[]  = { "chromium", NULL };
+static const char *kittycmd[]  = { "kitty", NULL };
 static const char *screenkeytoggle[]  = { "/home/anonymous/scripts/sck-tog.sh", NULL };
-//static const char *wpcmd[]  = { "bash","/home/anonymous/scripts/wp-toggle.sh", NULL };
+//static const char *wpcmd[]  = { "sh","/home/anonymous/scripts/wp-toggle.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 static const char *suspendcmd[]  = { "/home/anonymous/scripts/suspend.sh", NULL };
@@ -107,20 +110,22 @@ static const char *resolution2[] = { "/home/anonymous/scripts/resolution.1080p.s
 
 static Key keys[] = {
 	/* modifier		                key                   function            argument */
-	{ MODKEY,		                XK_s,			    spawn,                {.v = dmenucmd } },
-	{ MODKEY,		                XK_Return,		    spawn,                {.v = termcmd } },
-	{ MODKEY|ShiftMask,	            XK_c,			    spawn,                {.v = browsercmd } },
-	{ MODKEY,		                XK_l,			    spawn,                {.v = suspendcmd } },//sleep
-	{ 0,			      XF86XK_AudioLowerVolume,      spawn,                {.v = downvol } },
-	{ 0,			                XF86XK_AudioMute,	spawn,                {.v = mutevol } },
-	{ 0,			      XF86XK_AudioRaiseVolume,      spawn,                {.v = upvol   } },
-//	{ MODKEY,                       XK_bracketleft,     spawn,                {.v = downvol } },
-//	{ MODKEY,                       XK_backslash,		spawn,                {.v = mutevol } },
-//	{ MODKEY,                       XK_bracketright,	spawn,                {.v = upvol   } },
-	{ MODKEY,	                    XF86XK_Phone,       spawn,                {.v = screenkeytoggle } },
-//	{ Mod1Mask,		                XF86XK_Phone,       spawn,                {.v = wpcmd } },
-	{ MODKEY|ControlMask,		                XF86XK_Phone,       spawn,                {.v = resolution1 } },
-	{ MODKEY|ShiftMask,		    XF86XK_Phone,       spawn,                {.v = resolution2 } },
+	//{ MODKEY,		                XK_s,			spawn,                {.v = dmenucmd } },
+	{ MODKEY,		                XK_s,			spawn,                {.v = roficmd } },
+	{ MODKEY,		                XK_Return,		spawn,                {.v = termcmd } },
+	{ MODKEY|ShiftMask,	                XK_c,			spawn,                {.v = browsercmd } },
+	{ MODKEY,	                        XK_k,			spawn,                {.v = kittycmd } },
+	{ MODKEY,		                XK_l,			spawn,                {.v = suspendcmd } },//sleep
+	{ 0,	             XF86XK_AudioLowerVolume,                   spawn,                {.v = downvol } },
+	{ 0,			    XF86XK_AudioMute,	                spawn,                {.v = mutevol } },
+	{ 0,		     XF86XK_AudioRaiseVolume,                   spawn,                {.v = upvol   } },
+//	{ MODKEY,                       XK_bracketleft,                 spawn,                {.v = downvol } },
+//	{ MODKEY,                       XK_backslash,		        spawn,                {.v = mutevol } },
+//	{ MODKEY,                       XK_bracketright,                spawn,                {.v = upvol   } },
+	{ MODKEY,	                    XF86XK_Phone,               spawn,                {.v = screenkeytoggle } },
+//	{ Mod1Mask,		                XF86XK_Phone,           spawn,                {.v = wpcmd } },
+	{ MODKEY|ControlMask,		    XF86XK_Phone,       spawn,                {.v = resolution1 } },
+	{ MODKEY|ShiftMask,		        XF86XK_Phone,       spawn,                {.v = resolution2 } },
 	{ 0,			                XK_Print,           spawn,	    	      {.v = screenshotcmd } },
 	{ MODKEY|ShiftMask,	            XK_w,			    rotatestack,	      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_s,			    rotatestack,	      {.i = -1 } },
@@ -136,7 +141,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,               restorewin,           {0} },
 //	{ MODKEY|ControlMask,           XK_h,               hideotherwins,        {0}},
 //	{ MODKEY|ShiftMask|ControlMask, XK_h,               restoreotherwins,     {0}},
-//	{ MODKEY|ShiftMask,             XK_n,               zoom,                 {0} },
+	{ MODKEY|ShiftMask,             XK_f,               zoom,                 {0} },
 	{ MODKEY,                       XK_Tab,             view,                 {0} },
 	{ MODKEY|ShiftMask,             XK_q,               killclient,           {0} },
 	{ MODKEY|ShiftMask,             XK_n,               incnmaster,	          {.i = +1 } },
